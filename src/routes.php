842b1fad266cc3022dc6adcccad7100919a2985e
Paul Wilkin <paul.wilkin@fitsolve.co.uk>
2022-03-21 11:45:21 +0000

    Revert "CRON-8104 Update routes.php"

--- src/routes.php
+++ src/routes.php
@@ -3,5 +3,5 @@
 $loginRoute = Config::get('saml.routes.login', 'login');
 $logoutRoute = Config::get('saml.routes.logout', 'logout');
 
-Route::get($loginRoute, 'SamlController@login')->name('login');
-Route::get($logoutRoute, 'SamlController@logout')->name('logout');
+Route::get($loginRoute, 'SamlController@login');
+Route::get($logoutRoute, 'SamlController@logout');
